01728b65849b77e5ba4416f9517b59f09e0b9b59
ghostogre <yingjhpad@icloud.com>
2020-06-18 00:38:22 +0800

    fix 测试用例

--- 算法/code/__tests__/betterReverse.test.js
+++
@@ -1,5 +0,0 @@
-import betterReverse from '../src/str/betterReverse'
-
-test('-13579 翻转成 -97531 欧几得', () => {
-  expect(betterReverse(-13579)).toBe(-97531)
-})
--- 算法/code/__tests__/reverse.test.js
+++ 算法/code/__tests__/reverse.test.js
@@ -1,5 +1,13 @@
 import reverse from '../src/str/reverse'
+import betterReverse from '../src/str/betterReverse'
 
-test('-13579 翻转成 -97531', () => {
-  expect(reverse(-13579)).toBe(-97531)
+describe('字符串翻转：', () => {
+  // 使用it而不是test，test只会显示第一个测试的运行时间
+  it('-13579 翻转成 -97531 欧几得', () => {
+    expect(betterReverse(-13579)).toBe(-97531)
+  })
+
+  it('-13579 翻转成 -97531', () => {
+    expect(reverse(-13579)).toBe(-97531)
+  })
 })
